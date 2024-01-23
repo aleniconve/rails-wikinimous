1da58fc4845f252d4e5169af5bb09a9161da523e
Alejandro Contreras <contrerasx@gmail.com>
2024-01-23 16:53:17 +0100

    routes, controller and methods done, still missing the views, challenge not completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  content = Faker::Esport.player
+  title = Faker::Name.unique.name
+  Article.create(title: title, content: content)
+end
